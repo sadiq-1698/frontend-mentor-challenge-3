1f1c0a45bc2422bd63bf0bc7c3080d7c298df1d1
sadiq-1698 <mohammedsadiq1698@gmail.com>
2020-10-12 10:54:05 +0530

    Complete

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,27 @@
-let menuItem = document.getElementsById('');
+let menuModal = document.getElementById('menu-modal');
+let bodyElement = document.getElementById('body');
+let oneMorebodyElement = document.getElementById('one-more');
+let menuIcon = document.getElementById('menu-icon');
+let isMenuModalOpen = false;
+
+
+function displayMenuModal(){
+    isMenuModalOpen = !isMenuModalOpen;
+    if(isMenuModalOpen){
+        menuModal.style.display = "block";
+        bodyElement.style.opacity = 0.2;    
+        bodyElement.style.backgroundColor = "black";
+        oneMorebodyElement.style.opacity = 0.2;
+        oneMorebodyElement.style.backgroundColor = "black";
+        menuIcon.classList.remove("fa-bars");
+        menuIcon.classList.add("fa-times");
+    }else{
+        menuModal.style.display = "none";
+        bodyElement.style.opacity = 1;    
+        oneMorebodyElement.style.opacity = 1;
+        bodyElement.style.backgroundColor = "hsl(220, 16%, 96%)";
+        oneMorebodyElement.style.backgroundColor = "hsl(0, 0%, 98%)";
+        menuIcon.classList.remove("fa-times");
+        menuIcon.classList.add("fa-bars");
+    }
+}
